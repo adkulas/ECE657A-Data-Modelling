98d94e1087e5cbf70e6a51ef6b494af33c01f9df
Adam Kulas <adam.kulas1@gmail.com>
2019-01-09 13:21:36 -0500

    homework one summary stats and graphs

--- HW1/homework1.py
+++ HW1/homework1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import seaborn as sns
 
 df = pd.read_csv('data/breast-cancer-wisconsin/breast-cancer-wisconsin.data', header=None)
 columns = ['Sample_code_number',
@@ -12,4 +13,24 @@ columns = ['Sample_code_number',
            'Normal_Nucleoli',
            'Mitoses', 
            'Class']  
+# df.set_index('Sample_code_number')
 df.columns = columns
+
+summary_stats = [df.mean(), df.skew(), df.std(), df.var()]
+results_df = pd.DataFrame()
+results_df['mean'] = df.mean()
+results_df['skew'] = df.skew()
+results_df['std_dev'] = df.std()
+results_df['variance'] = df.var()
+
+results_df = results_df.T
+print(results_df.head())
+
+correlations = df.corr(method='pearson')
+print(correlations.head(100))
+
+benign = df.loc[df['Class'] == 2]
+malignant = df.loc[df['Class'] == 4]
+print(benign.head())
+
+sns.distplot(benign.iloc[:,2])
